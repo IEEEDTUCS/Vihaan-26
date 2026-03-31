c0dc91bd0d85d742a8e18e05c6128b5056055c8e
ManitVig <manitvig@gmail.com>
2026-03-31 23:22:12 +0530

    Backend setup

---
+++ backend/src/app.ts
@@ -0,0 +1,13 @@
+import express, {Request, Response} from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello from Express + TypeScript 🚀");
+});
+
+export default app;
