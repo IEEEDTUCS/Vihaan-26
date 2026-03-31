a49c7b1de66a98b36b1af50992eeb2cab808ad2e
ManitVig <manitvig@gmail.com>
2026-03-31 23:31:37 +0530

    mongodb connection setup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,14 @@
 import express, {Request, Response} from "express";
+import {connectDB} from "./config/db";
 
 const app = express();
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
+
+// connect DB
+connectDB();
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello from Express + TypeScript 🚀");
